d9fb0e89d15511e2b3c41b1b4e8a76fc9e739360
Ayush <ayushmanojgupta24@gmail.com>
2020-04-30 10:55:35 +0530

    Added Case statement to calculate employee wage in EmpWageComputation.sh

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -6,6 +6,7 @@ isPresent=1
 randomCheck=$((RANDOM%2))
 timeCheck=$((RANDOM%2))
 isFullTime=1
+isParttime=0
 
 #constants
 WAGE_PER_HOUR=20
@@ -14,17 +15,18 @@ EMP_HOUR_PART_TIME=8
 
 if [ $isPresent  == $randomCheck ]
 then
-	if [ $isFullTime == $timeCheck ]
-	then
+	case $timeCheck in $isFullTime )
 		echo "Employee is present and is a FullTime Employee"
 		wagePerDay=$(( $WAGE_PER_HOUR * $$EMP_HOUR_FULL_TIME ))
 		echo "Wage Per Day" $wagePerDay
-	else
+					;;
+	$isPartTime )
 		echo "Employee is present and is a PartTime Employee"
 
 		wagePerDay=$(( $WAGE_PER_HOUR*$EMP_HOUR_PART_TIME ))
         	echo "Wage Per Day" $wagePerDay
-	fi
+					;;
+	esac
 else
 		echo "Employee is absent"
 fi
