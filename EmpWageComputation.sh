7ec8f0b05a13f66c645d1f62883eab5e4c840213
Ayush <ayushmanojgupta24@gmail.com>
2020-04-30 10:45:36 +0530

    Added condition to calculate part time employee wage in EmpWageComputation.sh

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -4,17 +4,27 @@
 #Variable
 isPresent=1
 randomCheck=$((RANDOM%2))
+timeCheck=$((RANDOM%2))
+isFullTime=1
 
 #constants
 WAGE_PER_HOUR=20
 EMP_HOUR_FULL_TIME=8
+EMP_HOUR_PART_TIME=8
 
 if [ $isPresent  == $randomCheck ]
 then
-	echo "Employee is present"
-	wagePerDay=$(( $WAGE_PER_HOUR * $EMP_HOUR_FULL_TIME ))
-	echo $wagePerDay
+	if [ $isFullTime == $timeCheck ]
+	then
+		echo "Employee is present and is a FullTime Employee"
+		wagePerDay=$(( $WAGE_PER_HOUR * $$EMP_HOUR_FULL_TIME ))
+		echo "Wage Per Day" $wagePerDay
+	else
+		echo "Employee is present and is a PartTime Employee"
 
+		wagePerDay=$(( $WAGE_PER_HOUR*$EMP_HOUR_PART_TIME ))
+        	echo "Wage Per Day" $wagePerDay
+	fi
 else
-	echo "Employee is Absent"
+		echo "Employee is absent"
 fi
