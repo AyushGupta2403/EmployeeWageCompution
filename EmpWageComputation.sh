3d8c904b5fa79653adf96850b38a7ee215faaea7
Ayush <ayushmanojgupta24@gmail.com>
2020-04-30 10:29:51 +0530

    Daily Wage calculation added to EmpWageComputation.sh

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -5,9 +5,16 @@
 isPresent=1
 randomCheck=$((RANDOM%2))
 
+#constants
+WAGE_PER_HOUR=20
+EMP_HOUR_FULL_TIME=8
+
 if [ $isPresent  == $randomCheck ]
 then
 	echo "Employee is present"
+	wagePerDay=$(( $WAGE_PER_HOUR * $EMP_HOUR_FULL_TIME ))
+	echo $wagePerDay
+
 else
 	echo "Employee is Absent"
 fi
